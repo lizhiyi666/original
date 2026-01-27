92f201872197e455bc86ba4a2b78af6e922c2ffc
lizhiyi666 <1954242167@qq.com>
2026-01-27 11:43:02 +0000

    new run

--- sample_evaluation.sh
+++ sample_evaluation.sh
@@ -13,19 +13,19 @@ python sample.py \
   --run_id "$RUN_ID" \
   --use_constraint_projection \
   --debug_constraint_projection \
-  --projection_frequency 2 \
-  --projection_outer_iters 20 \
-  --projection_inner_iters 50\
+  --projection_frequency 4 \
+  --projection_outer_iters 100 \
+  --projection_inner_iters 100\
   --projection_tau 0 \
   --projection_lambda 1.0 \
-  --projection_eta 0.8 \
+  --projection_eta 0.4 \
   --projection_mu 1.0 \
   --projection_mu_max 1000.0 \
   --projection_mu_alpha 2.0 \
   --projection_delta_tol 0.000001\
   --use_gumbel_softmax \
   --gumbel_temperature 0.1 \
-  --projection_last_k_steps 50 \
+  --projection_last_k_steps 40 \
   --projection_existence_weight 5.0 \
 
 python evaluation.py --datasets Istanbul_PO1 --task Stat --experiment_comments "$RUN_ID" &
